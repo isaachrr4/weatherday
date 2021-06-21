a4cde75e1eaf162890383f1bc4ea1ddb910d0721
isaachrr4 <80857536+isaachrr4@users.noreply.github.com>
2021-06-21 18:01:56 -0400

    adding functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
-var key ="";
-var city = "find city";
-var url = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=2962d44fd85312c91114977fc1b79714"
+
+var city = "";
+var url = "https://api.openweathermap.org/data/2.5/weather?q"
+var searchBtn
+var currentIndex
+var showTemp
+var wSpeed
+var showHumid
+var showWeather
+var searchCity
+
+
+// fetch for api key
+fetch("https://api.openweathermap.org/data/2.5/weather?q={Orlando}&appid={2962d44fd85312c91114977fc1b79714}")
+.then(response => response.json())
+.then(data => console.log(data));
+
+
+//Api key
+var APIKey ="2962d44fd85312c91114977fc1b79714"
+
+function searchCity () {
+    for (var i=0; i<searchCity.length; i++);
+}
 
 
